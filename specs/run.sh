4dcc28b630e8e1a3045257a65349e52bfd9815cb
Leo Choi <choimankin@gmail.com>
2018-01-28 11:49:03 +0800

    1. Done

---
+++ specs/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+declare -a specs=(
+    "BinarySearch.java"
+    "BinaryTree.java"
+    "BubbleSort.java"
+    "Factorial.java"
+    "LinearSearch.java"
+    "LinkedList.java"
+    "QuickSort.java"
+    "BubbleSort.java"
+    "TreeVisitor.java"
+    "MultiLineCommentUnexpectedEOF.java"
+    "UnmatchedMultiLineComment.java"
+    "InvalidCharacter.java"
+)
+
+for i in "${specs[@]}"
+do
+   java Main < "../specs/$i"
+   # or do whatever with individual element of the array
+done
